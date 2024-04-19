85ffdcd760096e22751a53c8f189d9f7032e2b3d
Pratham <chudasamapratham142@gmail.com>
2024-04-19 17:01:48 +0530

    made all articles section

--- client/src/components/AllArticles.js
+++ client/src/components/AllArticles.js
@@ -1,63 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const AllArticles = () => {
-  const [hashes, setHashes] = useState([]);
-  const [articles, setArticles] = useState([]);
+	const [hashes, setHashes] = useState([]);
+	const [articles, setArticles] = useState([]);
 
-  async function fetchHashes() {
-    try {
-      const response = await fetch('http://localhost:5000');
-      if (!response.ok) {
-        throw new Error(`Error fetching articles: ${response}`);
-      }
-      const data = await response.json();
-      setHashes(data);
-    } catch (error) {
-      console.error('Error fetching articles:', error);
-    }
-  }
+	async function fetchHashes() {
+		try {
+			const response = await fetch("http://localhost:5000");
+			if (!response.ok) {
+				throw new Error(`Error fetching articles: ${response}`);
+			}
+			const data = await response.json();
+			setHashes(data);
+		} catch (error) {
+			console.error("Error fetching articles:", error);
+		}
+	}
 
-  async function fetchArticleContent(hash) {
-    try {
-      const response = await fetch(`https://gateway.pinata.cloud/ipfs/${hash}`);
-      if (!response.ok) {
-        throw new Error(`Error fetching article content: ${response}`);
-      }
-      const content = await response.json();
-      const fullContent = content.content;
-      setArticles((prevArticles) => [
-        ...prevArticles,
-        { title: content.title, content: fullContent, hash },
-      ]);
-    } catch (error) {
-      console.error(`Error fetching article content for hash ${hash}:`, error);
-    }
-  }
+	async function fetchArticleContent(hash) {
+		try {
+			const response = await fetch(
+				`https://gateway.pinata.cloud/ipfs/${hash}`
+			);
+			if (!response.ok) {
+				throw new Error(`Error fetching article content: ${response}`);
+			}
+			const content = await response.json();
+			const fullContent = content.content;
+			console.log("fullContent:", fullContent);
+			setArticles((prevArticles) => [
+				...prevArticles,
+				{ title: content.title, content: fullContent, hash },
+			]);
+		} catch (error) {
+			console.error(
+				`Error fetching article content for hash ${hash}:`,
+				error
+			);
+		}
+	}
 
-  useEffect(() => {
-    fetchHashes();
-  }, []);
+	useEffect(() => {
+		fetchHashes();
+	}, []);
 
-  useEffect(() => {
-    // Fetch content for each hash
-    hashes.forEach((hash) => fetchArticleContent(hash));
-  }, [hashes]);
+	useEffect(() => {
+		// Fetch content for each hash
+		hashes.forEach((hash) => fetchArticleContent(hash));
+	}, [hashes]);
 
-  return (
-    <div>
-      <div className="articles-heading-container">
-        <h2 className='articles-heading chomsky'>News Articles</h2>
-      </div>
-      {articles.map((content, index) => (
-        <div className='all-articles-article-container' key={index}>
-          <Link className='all-articles-article' to={`/article/${content.hash}`} state={{ articleContent: content }}>
-            {content.title}
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			<div className="articles-heading-container">
+				<h2 className="articles-heading chomsky">News Articles</h2>
+			</div>
+			<div className="all-articles-container">
+				{articles.map(
+					(content, index) =>
+						content.title && (
+							<div key={index}>
+								<Link
+									className="article-link"
+									to={`/article/${content.hash}`}
+									state={{ articleContent: content }}
+								>
+									<i class="fa-solid fa-arrow-up-right-from-square"></i>
+									{content.title}
+								</Link>
+							</div>
+						)
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default AllArticles;
